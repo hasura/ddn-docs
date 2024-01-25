8347519f21d681311f7c68ab9186caad8d7282f7
surendran <surendran@hasura.io>
2024-01-25 15:09:05 +0530

    homepage ui fixes

--- src/theme/Navbar/Content/index.js
+++ src/theme/Navbar/Content/index.js
@@ -50,7 +50,7 @@ function NavbarContentLayout({left, right, searchBarItem}) {
       )}
       <div className="navbar__items navbar__items--right">
         {right}
-        <a href='https://cloud.hasura.io/login' className={'navbar__item navbar__link flex items-center ' + styles.navBarBtn}>
+        <a href='https://cloud.hasura.io/login' className={'navbar__item navbar__link ' + styles.navBarBtn}>
           Log In
           <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M7.5 15.5L12.5 10.5L7.5 5.5" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
