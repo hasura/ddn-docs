4b650d7d0b203a413e9e948a17c506af747cdb56
surendran82 <surendran@hasura.io>
2024-02-21 03:15:05 +0530

    V3 Docs ui updates (#267)
    Signed-off-by: Sean Park-Ross <sean@hasura.io>
    Co-authored-by: Sean Park-Ross <sean@hasura.io>

---
+++ src/components/CustomDocItem/index.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect } from 'react';
+import ActualDocItem from '@theme/DocItem';
+// import HasuraConBanner from '@site/src/components/HasuraConBanner';
+// import GraphQLWithHasuraBanner from '@site/src/components/GraphQLWithHasuraBanner';
+import CustomFooter from '@site/src/components/CustomFooter';
+import styles from './styles.module.scss';
+import { Redirect } from '@docusaurus/router';
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import { AiChatBot } from "@site/src/components/AiChatBot/AiChatBot";
+
+const CustomDocItem = props => {
+  useEffect(() => {
+    // This function is adds <wbr> tags to code blocks within a table
+    // wherever an _ or . is present. We do this since so many environment
+    // variables are incredibly long and the word breaks are not happening
+    // in a user-friendly way.
+    const tables = document.querySelectorAll('table');
+    tables.forEach(table => {
+      const cells = table.querySelectorAll('td');
+      cells.forEach(cell => {
+        const codeBlocks = cell.querySelectorAll('code');
+        codeBlocks.forEach(codeBlock => {
+          codeBlock.innerHTML = codeBlock.innerHTML.replace(/_/g, '_<wbr>');
+          codeBlock.innerHTML = codeBlock.innerHTML.replace(/\./g, '.<wbr>');
+          console.log(codeBlock.innerHTML);
+        });
+      });
+    });
+
+    // dynamically updating the pg prop for the getting started cta
+    function updateGettingStartedParam() {
+      const linkElement = document.querySelector('.navbar__link.nav-link_getting-started');
+
+      if (linkElement) {
+        let page = props.location.pathname;
+        page = page.slice(0, -1);
+        page = page.slice(1);
+        page = page.replace('docs/3.0/', 'docs_v3_').replace(/\//g, '_');
+
+        const href = linkElement.getAttribute('href');
+        const newHref = href.replace(/pg=docs_v3_([^&]+)/, `pg=${page}`);
+        linkElement.setAttribute('href', newHref);
+      }
+    }
+
+    updateGettingStartedParam();
+  }, []);
+
+  useEffect(() => {
+    // check for the browser
+    if (typeof window !== 'undefined') {
+      let directives = document.body.querySelectorAll('[class*="code_heading"]');
+      console.log(directives);
+      if (directives.length > 0) {
+        for (let i = 0; i < directives.length; i++) {
+          directives[i].setAttribute('data-step', i + 1);
+        }
+      }
+      let steps = document.body.querySelectorAll('[class*="step_container"]');
+      // get the firs element and add a margin-top of 50px
+      if (steps.length > 0) {
+        steps[0].setAttribute('style', 'margin-top: 75px');
+      }
+    }
+  });
+
+  // redirect them to the index if they attempt to directly navigate to a path with
+  // _heading_ in it
+  if (props.location.pathname.includes('_heading_')) {
+    return <Redirect to="/docs/index/" />;
+  }
+
+  return (
+    <div
+      className={
+        props.location.pathname === `/index/`
+          ? `custom_doc_item_wrapper custom_doc_item_wrapper-x-wide`
+          : `custom_doc_item_wrapper ${styles['custom_doc_item_wrapper']}`
+      }
+    >
+      <ActualDocItem {...props} />
+      <div
+        className={
+          props.location.pathname === `/index/` || props.location.pathname.includes('overview')
+            ? `custom_doc_item_footer-x-wide`
+            : styles['custom_doc_item_footer']
+        }
+      >
+        {/* <GraphQLWithHasuraBanner /> */}
+        <BrowserOnly fallback={<div>Loading...</div>}>
+          {() => <AiChatBot/>}
+        </BrowserOnly>
+        <CustomFooter />
+      </div>
+    </div>
+  );
+};
+
+export default CustomDocItem;
---
+++ src/theme/DocSidebarItem/utils.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+
+import CloudDark from '@site/static/icons/cloud-light.svg';
+import CloudLight from '@site/static/icons/cloud-dark.svg';
+
+import Introduction from '@site/static/icons/award-02.svg';
+import Basics from '@site/static/icons/book-open-01.svg';
+import GettingStarted from '@site/static/icons/home-smile.svg';
+import Auth from '@site/static/icons/shield-tick.svg';
+import Connectors from '@site/static/icons/event-triggers.svg';
+import DataDomainModeling from '@site/static/icons/database-01.svg';
+import GraphQLAPI from '@site/static/icons/graphql-logo.svg';
+import CiCd from '@site/static/icons/upload-cloud-02.svg';
+import HasuraCLI from '@site/static/icons/terminal-square.svg';
+import Observability from '@site/static/icons/eye.svg';
+import Glossary from '@site/static/icons/box.svg';
+import Quickstart from '@site/static/icons/speedometer-04.svg';
+import SupergraphModeling from '@site/static/icons/cpu-chip-01.svg';
+import Faq from '@site/static/icons/help-square.svg';
+import Community from '@site/static/icons/announcement-02.svg';
+
+import styles from '@site/src/theme/DocSidebarItem/Category/styles.module.css';
+import { useColorMode } from '@docusaurus/theme-common';
+
+export function addIconsToLabel(label, className) {
+  const { colorMode } = useColorMode();
+  const [definedColorMode, setDefinedColorMode] = useState('');
+
+  useEffect(() => {
+    setDefinedColorMode(colorMode);
+  }, [colorMode]);
+
+  const isDarkMode = definedColorMode === 'dark';
+
+  const cloudIcon = isDarkMode ? <CloudDark /> : <CloudLight />;
+
+  // Conditional rendering for sidebar icons
+  let icons;
+  switch (className) {
+    case 'introduction-icon':
+      icons = <Introduction />;
+      break;
+    case 'basics-icon':
+      icons = <Basics />;
+      break;
+    case 'getting-started-icon':
+      icons = <GettingStarted />;
+      break;
+    case 'auth-icon':
+      icons = <Auth />;
+      break;
+    case 'connectors-icon':
+      icons = <Connectors />;
+      break;
+    case 'data-domain-modeling-icon':
+      icons = <DataDomainModeling />;
+      break;
+    case 'graphQL-api-icon':
+      icons = <GraphQLAPI />;
+      break;
+    case 'ci-cd-icon':
+      icons = <CiCd />;
+      break;
+    case 'hasura-cli-icon':
+      icons = <HasuraCLI />;
+      break;
+    case 'observability-icon':
+      icons = <Observability />;
+      break;
+    case 'glossary-icon':
+      icons = <Glossary />;
+      break;
+    case 'quickstart-icon':
+      icons = <Quickstart />;
+      break;
+    case 'supergraph-modeling-icon':
+      icons = <SupergraphModeling />;
+      break;
+    case 'faq-icon':
+      icons = <Faq />;
+      break;
+    case 'community-icon':
+      icons = <Community />;
+      break;
+  }
+  
+  return (
+    <div className={styles['sidebar_link_wrapper']}>
+      {icons} {label} 
+    </div>
+  );
+}
