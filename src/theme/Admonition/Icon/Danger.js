6a9d4eedde4caf9bc83bf1aadd2333bfbfe89cc4
surendran <surendran@hasura.io>
2024-03-04 12:05:45 +0530

    admonitions icons updated

--- src/theme/Admonition/Icon/Danger.js
+++ src/theme/Admonition/Icon/Danger.js
@@ -1,11 +1,15 @@
 import React from 'react';
 export default function AdmonitionIconDanger(props) {
   return (
-    <svg viewBox="0 0 12 16" {...props}>
-      <path
-        fillRule="evenodd"
-        d="M5.05.31c.81 2.17.41 3.38-.52 4.31C3.55 5.67 1.98 6.45.9 7.98c-1.45 2.05-1.7 6.53 3.53 7.7-2.2-1.16-2.67-4.52-.3-6.61-.61 2.03.53 3.33 1.94 2.86 1.39-.47 2.3.53 2.27 1.67-.02.78-.31 1.44-1.13 1.81 3.42-.59 4.78-3.42 4.78-5.56 0-2.84-2.53-3.22-1.25-5.61-1.52.13-2.03 1.13-1.89 2.75.09 1.08-1.02 1.8-1.86 1.33-.67-.41-.66-1.19-.06-1.78C8.18 5.31 8.68 2.45 5.05.32L5.03.3l.02.01z"
-      />
+    <svg className='admonition-icon' width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g clip-path="url(#clip0_15583_5564)">
+      <path d="M4.10768 4.10841L15.891 15.8917M15.891 4.10841L4.10763 15.8917M18.3327 10.0001C18.3327 14.6025 14.6017 18.3334 9.99935 18.3334C5.39698 18.3334 1.66602 14.6025 1.66602 10.0001C1.66602 5.39771 5.39698 1.66675 9.99935 1.66675C14.6017 1.66675 18.3327 5.39771 18.3327 10.0001Z" stroke="#1F2A37" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" fill='none'/>
+      </g>
+      <defs>
+      <clipPath id="clip0_15583_5564">
+      <rect width="20" height="20" fill="white"/>
+      </clipPath>
+      </defs>
     </svg>
   );
 }
