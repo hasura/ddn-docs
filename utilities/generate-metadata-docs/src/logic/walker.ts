016658a5c4bafbbfd920ad9882649b8ab1aee7b9
Rob Dominguez <rob.dominguez@hasura.io>
2024-03-11 16:46:08 -0500

    wip: stopping point:

--- utilities/generate-metadata-docs/src/logic/walker.ts
+++ utilities/generate-metadata-docs/src/logic/walker.ts
@@ -9,40 +9,24 @@ const parentSchema: JSONSchema7 = JSON.parse(readFileSync('./schema.json', 'utf8
 type UniqueIdentifier = string;
 
 // TODO: generateMarkdownForJsonSchema
-// object.const -> returnable
-// object.enum -> returnable
+// ✅ object.const -> returnable
+// ✅ object.enum -> returnable
 // ☝️ not recursive
 // object.type === array -> array.items returnable, looping
-// object.type === object && object.properties -> returnable
-// object.type === object && object.additionalProperties -> returnable
-// object.type === scalar -> returnable
-// object.oneOf, anyOf -> returnable OR type This is the piped type of output
-// object.allOf -> special case, has to be one of the fancy properties
+// ✅ object.type === object && object.properties -> returnable
+// ✅ object.type === object && object.additionalProperties -> returnable
+// ✅ object.type === scalar -> returnable
+// ✅ object.oneOf, anyOf -> returnable OR type This is the piped type of output
+// ✅ object.allOf -> special case, has to be one of the fancy properties
 export function handleSchemaDefinition(metadataObject: JSONSchema7Definition): JSONSchema7 {
-  let md = ``;
+  // Deal with const
+  handleConst(metadataObject);
+
+  // Deal with enum
+  handleEnum(metadataObject);
 
   // Deal with scalars
-  const scalars = ['string', 'int', 'number'];
-  scalars.map(scalarType => {
-    if (metadataObject.type && metadataObject.type.toString() === scalarType) {
-      return metadataObject;
-    }
-  });
-
-  // Deal with metadata object that is of type object and has properties
-  if (metadataObject.type === 'object' && metadataObject.properties) {
-    return metadataObject;
-  }
-
-  if (metadataObject.type === 'object' && metadataObject.additionalProperties) {
-    return metadataObject;
-  }
-
-  // Add a title if it's available
-  md += addTitle(metadataObject);
-
-  // Add a description if it's available
-  md += addDescription(metadataObject);
+  handleScalars(metadataObject);
 
   // Deal with properties
   handleProperties(metadataObject);
@@ -65,36 +49,63 @@ export function handleSchemaDefinition(metadataObject: JSONSchema7Definition): J
   // Deal with anyOf
   simplifyAnyOf(metadataObject);
 
-  console.log(md);
-
   return metadataObject;
+}
+
+function handleConst(metadataObject: JSONSchema7Definition) {
+  if (metadataObject.const) {
+    console.log(`Encountered const: ${metadataObject.const}`);
+    return metadataObject;
+  }
+}
+
+function handleEnum(metadataObject: JSONSchema7Definition) {
+  if (metadataObject.enum) {
+    console.log(`Encountered enum: ${metadataObject.enum}`);
+    return metadataObject;
+  }
+}
+
+function handleScalars(metadataObject: JSONSchema7Definition) {
+  if (metadataObject.type) {
+    const scalarTypes = [`string`, `integer`, `number`];
+    if (scalarTypes.includes(metadataObject.type.toString())) {
+      return metadataObject;
+    }
+  }
 }
 
 function handleOneOf(metadataObject: JSONSchema7Definition) {
   if (metadataObject.oneOf) {
     metadataObject.oneOf.map(option => {
-      handleSchemaDefinition(option);
+      const oneOf = handleSchemaDefinition(option);
+      console.log(oneOf);
     });
   }
 }
 
 function handleProperties(metadataObject: JSONSchema7Definition) {
-  if (metadataObject.properties) {
-    for (const [key, value] of Object.entries(metadataObject.properties)) {
-      handleSchemaDefinition(value);
+  if (metadataObject.type && metadataObject.type === 'object') {
+    if (metadataObject.properties) {
+      for (const [key, value] of Object.entries(metadataObject.properties)) {
+        const prop = handleSchemaDefinition(value);
+        console.log(prop);
+      }
     }
   }
 }
 
 function handleRef(metadataObject: JSONSchema7Definition) {
   if (metadataObject.$ref) {
-    console.log(`Handling ref...`);
     const ref = metadataObject.$ref;
     const parsedRef = ref.split('/').pop();
     const referencedObject = parentSchema.definitions[parsedRef];
     // This keeps us from dealing with external refs like http...
     if (referencedObject != undefined) {
-      handleSchemaDefinition(referencedObject);
+      const newReference = handleSchemaDefinition(referencedObject);
+      console.log(newReference);
+    } else {
+      console.warn(`This is a non-local definition: ${ref}`);
     }
   }
 }
@@ -102,7 +113,8 @@ function handleRef(metadataObject: JSONSchema7Definition) {
 function handleAllOf(metadataObject: JSONSchema7Definition) {
   if (metadataObject.allOf) {
     for (const [key, value] of Object.entries(metadataObject.allOf)) {
-      handleSchemaDefinition(value);
+      const allOf = handleSchemaDefinition(value);
+      console.log(allOf);
     }
   }
 }
@@ -111,17 +123,21 @@ function handleItems(metadataObject: JSONSchema7Definition) {
   if (metadataObject.items) {
     if (Array.isArray(metadataObject.items)) {
       metadataObject.items.forEach(item => {
-        handleRef(item);
+        const referencedItem = handleRef(item);
+        console.log(referencedItem);
       });
     }
   }
 }
 
 function handleAdditionalProperties(metadataObject: JSONSchema7Definition) {
-  if (metadataObject.additionalProperties) {
-    for (const [key, value] of Object.entries(metadataObject.additionalProperties)) {
-      if (key === '$ref') {
-        handleRef({ $ref: value });
+  if (metadataObject.type === 'object') {
+    if (metadataObject.additionalProperties) {
+      for (const [key, value] of Object.entries(metadataObject.additionalProperties)) {
+        if (key === '$ref') {
+          const additionalProp = handleRef({ $ref: value });
+          console.log(additionalProp);
+        }
       }
     }
   }
@@ -132,7 +148,8 @@ function simplifyAnyOf(metadataObject: JSONSchema7Definition) {
     const simplifiedAnyOf = metadataObject.anyOf.filter(obj => obj.type != 'null');
     simplifiedAnyOf.map(item => {
       if (item['$ref']) {
-        handleRef(simplifiedAnyOf[0]);
+        const anyOf = handleRef(simplifiedAnyOf[0]);
+        console.log(anyOf);
       }
     });
   }
