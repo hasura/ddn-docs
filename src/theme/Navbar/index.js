d32db857167b08b77dd339191d7b183cfe5cdb0f
Sean Park-Ross <sean@hasura.io>
2024-03-28 17:54:23 +0000

    Remove from Navbar

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import NavbarLayout from '@theme/Navbar/Layout';
 import NavbarContent from '@theme/Navbar/Content';
-import { AiChatBot } from '@site/src/components/AiChatBot/AiChatBot';
-import BrowserOnly from '@docusaurus/BrowserOnly';
 export default function Navbar() {
   return (
     <NavbarLayout>
