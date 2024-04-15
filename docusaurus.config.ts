ebbe4b76fd749730559526aea321a6075644e854
Sean Park-Ross <sean@hasura.io>
2024-04-15 12:17:03 +0100

    WIP

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/3.0',
+  baseUrl: process.env.CF_PAGES === '1' ? '/' : '/docs/3.0',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
