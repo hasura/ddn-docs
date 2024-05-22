86754e5d956139f1d8d3c20aeae6c96bb17ac9a8
Rob Dominguez <rob.dominguez@hasura.io>
2024-05-22 16:03:49 -0500

    Add Posthog configuration in non-pages directory

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import NavbarLayout from '@theme/Navbar/Layout';
 import NavbarContent from '@theme/Navbar/Content';
+import UserFetcher from './Posthog/index';
 export default function Navbar() {
   return (
     <NavbarLayout>
+      <UserFetcher />
       <NavbarContent />
     </NavbarLayout>
   );
