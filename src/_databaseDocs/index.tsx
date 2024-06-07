5989eab4abe443024b5e337893479bb35db31a9c
Rob Dominguez <rob.dominguez@hasura.io>
2024-06-07 10:04:32 -0500

    Stub out pages for each connector

--- src/_databaseDocs/index.tsx
+++ src/_databaseDocs/index.tsx
@@ -3,6 +3,7 @@ import { useLocation } from '@docusaurus/router';
 import './styles.css';
 import PostgreSqlLogo from '@site/static/img/databases/logos/postgresql.png';
 import MongoDbLogo from '@site/static/img/databases/logos/mongodb.webp';
+import TypeScriptLogo from '@site/static/img/databases/logos/ts.png';
 import PostgreSqlConnect from '@site/src/_databaseDocs/_postgreSQL/_01-connect-a-data-source.mdx';
 import MongoDBConnect from '@site/src/_databaseDocs/_mongoDB/_01-connect-a-data-source.mdx';
 import PostgreSqlLink from '@site/src/_databaseDocs/_postgreSQL/_02-create-source-metadata.mdx';
@@ -11,6 +12,9 @@ import PostgreSqlExposition from '@site/src/_databaseDocs/_postgreSQL/_03-add-so
 import MongoDBExposition from '@site/src/_databaseDocs/_mongoDB/_03-add-source-entities.mdx';
 import PostgreSqlMutation from '@site/src/_databaseDocs/_postgreSQL/_09-mutate-data.mdx';
 import MongoDBMutation from '@site/src/_databaseDocs/_mongoDB/_09-mutate-data.mdx';
+import PostgreSqlDeployment from '@site/src/_databaseDocs/_postgreSQL/_deployment.mdx';
+import MongoDBDeployment from '@site/src/_databaseDocs/_mongoDB/_deployment.mdx';
+import TypeScriptDeployment from '@site/src/_databaseDocs/_typeScript/_deployment.mdx';
 
 const dataSources = {
   PostgreSQL: {
@@ -23,27 +27,32 @@ const dataSources = {
   },
   TypeScript: {
     name: 'TypeScript',
-    image: PostgreSqlLogo,
+    image: TypeScriptLogo,
   },
 };
 
 const DatabaseContentLoader = () => {
   const location = useLocation();
-  const [dbPreference, setDbPreference] = useState(null);
+  const [dbPreference, setDbPreference] = useState<string | null>(null);
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
     const dbParam = params.get('db');
+    const savedPreference = localStorage.getItem('dbPreference');
+    const isTypeScriptExcluded = location.pathname.includes('connect-to-data');
 
     if (dbParam && dataSources[dbParam]) {
       savePreference(dbParam);
-    } else {
-      const savedPreference = localStorage.getItem('dbPreference');
-      if (savedPreference) {
+    } else if (savedPreference) {
+      // If TypeScript is excluded and the saved preference is TypeScript, set preference to null
+      // to avoid text at the top of our component
+      if (isTypeScriptExcluded && savedPreference === 'TypeScript') {
+        setDbPreference(null);
+      } else {
         setDbPreference(savedPreference);
       }
     }
-  }, [location.search]);
+  }, [location.search, location.pathname]);
 
   const savePreference = (preference: string) => {
     localStorage.setItem('dbPreference', preference);
@@ -94,6 +103,17 @@ const DatabaseContentLoader = () => {
           default:
             return <div />;
         }
+      case 'deploy-a-connector':
+        switch (dbPreference) {
+          case 'PostgreSQL':
+            return <PostgreSqlDeployment />;
+          case 'MongoDB':
+            return <MongoDBDeployment />;
+          case 'TypeScript':
+            return <TypeScriptDeployment />;
+          default:
+            return <div />;
+        }
       default:
         return <div>Content not found...</div>;
     }
@@ -106,7 +126,7 @@ const DatabaseContentLoader = () => {
     <div>
       <div className="picker-wrapper">
         <small>
-          {dbPreference
+          {dbPreference && (!isTypeScriptExcluded || dbPreference !== 'TypeScript')
             ? `You are now reading ${dataSources[dbPreference].name}'s documentation`
             : "Select a data source's documentation"}
         </small>
@@ -131,7 +151,11 @@ const DatabaseContentLoader = () => {
           )}
         </div>
       </div>
-      {dbPreference ? getContent() : <div>Please select your database preference.</div>}
+      {dbPreference && (!isTypeScriptExcluded || dbPreference !== 'TypeScript') ? (
+        getContent()
+      ) : (
+        <div>Please select your database preference.</div>
+      )}
     </div>
   );
 };
