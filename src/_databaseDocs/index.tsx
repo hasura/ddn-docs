16b99d063d60671788b1ed41e1d1ce8c1c6a6789
Rob Dominguez <rob.dominguez@hasura.io>
2024-06-11 12:35:14 -0500

    Docs: Add ClickHouse-specific documentation (#435)

--- src/_databaseDocs/index.tsx
+++ src/_databaseDocs/index.tsx
@@ -6,17 +6,23 @@ import Icon from '@site/static/icons/event-triggers.svg';
 import PostgreSqlLogo from '@site/static/img/databases/logos/postgresql.png';
 import MongoDbLogo from '@site/static/img/databases/logos/mongodb.webp';
 import TypeScriptLogo from '@site/static/img/databases/logos/ts.png';
+import ClickHouseLogo from '@site/static/img/databases/logos/clickhouse-glyph.png';
 import PostgreSqlConnect from '@site/src/_databaseDocs/_postgreSQL/_01-connect-a-data-source.mdx';
 import MongoDBConnect from '@site/src/_databaseDocs/_mongoDB/_01-connect-a-data-source.mdx';
+import ClickHouseConnect from '@site/src/_databaseDocs/_clickHouse/_01-connect-a-data-source.mdx';
 import PostgreSqlLink from '@site/src/_databaseDocs/_postgreSQL/_02-create-source-metadata.mdx';
 import MongoDBLink from '@site/src/_databaseDocs/_mongoDB/_02-create-source-metadata.mdx';
+import ClickHouseLink from '@site/src/_databaseDocs/_clickHouse/_02-create-source-metadata.mdx';
 import PostgreSqlExposition from '@site/src/_databaseDocs/_postgreSQL/_03-add-source-entities.mdx';
 import MongoDBExposition from '@site/src/_databaseDocs/_mongoDB/_03-add-source-entities.mdx';
+import ClickHouseExposition from '@site/src/_databaseDocs/_clickHouse/_03-add-source-entities.mdx';
 import PostgreSqlMutation from '@site/src/_databaseDocs/_postgreSQL/_09-mutate-data.mdx';
 import MongoDBMutation from '@site/src/_databaseDocs/_mongoDB/_09-mutate-data.mdx';
+import ClickHouseMutation from '@site/src/_databaseDocs/_clickHouse/_09-mutate-data.mdx';
 import PostgreSqlDeployment from '@site/src/_databaseDocs/_postgreSQL/_deployment.mdx';
 import MongoDBDeployment from '@site/src/_databaseDocs/_mongoDB/_deployment.mdx';
 import TypeScriptDeployment from '@site/src/_databaseDocs/_typeScript/_deployment.mdx';
+import ClickHouseDeployment from '@site/src/_databaseDocs/_clickHouse/_deployment.mdx';
 
 const dataSources = {
   PostgreSQL: {
@@ -26,6 +32,10 @@ const dataSources = {
   MongoDB: {
     name: 'MongoDB',
     image: MongoDbLogo,
+  },
+  ClickHouse: {
+    name: 'ClickHouse',
+    image: ClickHouseLogo,
   },
   TypeScript: {
     name: 'TypeScript',
@@ -75,6 +85,8 @@ const DatabaseContentLoader = () => {
             return <PostgreSqlConnect />;
           case 'MongoDB':
             return <MongoDBConnect />;
+          case 'ClickHouse':
+            return <ClickHouseConnect />;
           default:
             return <div />;
         }
@@ -84,6 +96,8 @@ const DatabaseContentLoader = () => {
             return <PostgreSqlLink />;
           case 'MongoDB':
             return <MongoDBLink />;
+          case 'ClickHouse':
+            return <ClickHouseLink />;
           default:
             return <div />;
         }
@@ -93,6 +107,8 @@ const DatabaseContentLoader = () => {
             return <PostgreSqlExposition />;
           case 'MongoDB':
             return <MongoDBExposition />;
+          case 'ClickHouse':
+            return <ClickHouseExposition />;
           default:
             return <div />;
         }
@@ -102,6 +118,8 @@ const DatabaseContentLoader = () => {
             return <PostgreSqlMutation />;
           case 'MongoDB':
             return <MongoDBMutation />;
+          case 'ClickHouse':
+            return <ClickHouseMutation />;
           default:
             return <div />;
         }
@@ -111,6 +129,8 @@ const DatabaseContentLoader = () => {
             return <PostgreSqlDeployment />;
           case 'MongoDB':
             return <MongoDBDeployment />;
+          case 'ClickHouse':
+            return <ClickHouseDeployment />;
           case 'TypeScript':
             return <TypeScriptDeployment />;
           default:
