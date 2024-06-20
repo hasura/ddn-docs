0c4644937da2d05e1a91d154bd582d4ebd54ade6
Sean Park-Ross <94021366+seanparkross@users.noreply.github.com>
2024-06-20 11:16:27 +0100

    Getting Started Updates for new CLI (#443)

--- src/components/databaseDocs/index.tsx
+++ src/components/databaseDocs/index.tsx
@@ -25,10 +25,10 @@ import PostgreSqlMutate from '@site/docs/getting-started/_databaseDocs/_postgreS
 import MongoDBMutate from '@site/docs/getting-started/_databaseDocs/_mongoDB/_09-mutate-data.mdx';
 import ClickHouseMutate from '@site/docs/getting-started/_databaseDocs/_clickHouse/_09-mutate-data.mdx';
 
-import PostgreSqlDeploy from '@site/docs/getting-started/deployment/_databaseDocs/_postgreSQL/_deployment.mdx';
-import MongoDBDeploy from '@site/docs/getting-started/deployment/_databaseDocs/_mongoDB/_deployment.mdx';
-import TypeScriptDeploy from '@site/docs/getting-started/deployment/_databaseDocs/_typeScript/_deployment.mdx';
-import ClickHouseDeploy from '@site/docs/getting-started/deployment/_databaseDocs/_clickHouse/_deployment.mdx';
+import PostgreSqlDeploy from '@site/docs/getting-started/deployment/_databaseDocs/_postgreSQL/_03-deploy-a-connector.mdx';
+import MongoDBDeploy from '@site/docs/getting-started/deployment/_databaseDocs/_mongoDB/_03-deploy-a-connector.mdx';
+import TypeScriptDeploy from '@site/docs/getting-started/deployment/_databaseDocs/_typeScript/_03-deploy-a-connector.mdx';
+import ClickHouseDeploy from '@site/docs/getting-started/deployment/_databaseDocs/_clickHouse/_03-deploy-a-connector.mdx';
 
 const dataSources = {
   PostgreSQL: {
