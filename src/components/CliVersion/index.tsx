0293b23714c7605254931f0c8f77aa109db6dbaf
Rob Dominguez <rob.dominguez@hasura.io>
2024-07-16 11:53:02 -0500

    Docs: Add versioning to CLI install component
    This PR adds a dynamic version element to the CLI installation component.

---
+++ src/components/CliVersion/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import latestVersion from '../../../utilities/update-cli-version/latest-version.json';
+
+const Index: React.FC = () => {
+  const version = latestVersion.tag_name;
+
+  return (
+    <div>
+      <p>
+        You can download the CLI binary below. The latest version of the CLI is <kbd>{version}</kbd>. Please follow the
+        instructions for your system.
+      </p>
+    </div>
+  );
+};
+
+export default Index;
