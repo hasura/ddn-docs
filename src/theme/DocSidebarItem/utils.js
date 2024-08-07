4bfb60172f313829e8935aab4d022a04ef090168
Sooraj Sanker <soorajshankar@gmail.com>
2024-08-07 16:01:30 +0530

    Schema DIff Changes

--- src/theme/DocSidebarItem/utils.js
+++ src/theme/DocSidebarItem/utils.js
@@ -14,6 +14,7 @@ import CiCd from '@site/static/icons/upload-cloud-02.svg';
 import ProjectConfiguration from '@site/static/icons/dataflow-01.svg';
 import HasuraCLI from '@site/static/icons/terminal-square.svg';
 import Observability from '@site/static/icons/eye.svg';
+import Builds from '@site/static/icons/builds.svg';
 import Enterprise from '@site/static/icons/features/enterprise.svg';
 import Glossary from '@site/static/icons/box.svg';
 import Quickstart from '@site/static/icons/speedometer-04.svg';
@@ -75,6 +76,9 @@ export function addIconsToLabel(label, className) {
       break;
     case 'observability-icon':
       icons = <Observability />;
+      break;
+    case 'build-icon':
+      icons = <Builds />;
       break;
     case 'enterprise-icon':
       icons = <Enterprise />;
