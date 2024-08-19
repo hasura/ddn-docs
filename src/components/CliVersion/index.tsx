d46b62bded085020582aceb09388a430eb948fca
rikinsk <rikin@hasura.io>
2024-08-19 20:00:28 +0530

    add comment

--- src/components/CliVersion/index.tsx
+++ src/components/CliVersion/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 // import latestVersion from '../../../utilities/update-cli-version/latest-version.json';
 
 const Index: React.FC = () => {
+  //  TODO: fetch from the latest.json on CLI download bucket. https://graphql-engine-cdn.hasura.io/ddn/cli/<revision>/latest.json
   // const version = latestVersion.tag_name;
 
   return (
@@ -9,8 +10,7 @@ const Index: React.FC = () => {
       <p>
         {/*You can download the CLI binary below. The latest version of the CLI is <kbd>{version}</kbd>. Please follow the*/}
         {/*instructions for your system.*/}
-          You can download the CLI binary below. Please follow the
-          instructions for your system.
+        You can download the CLI binary below. Please follow the instructions for your system.
       </p>
     </div>
   );
