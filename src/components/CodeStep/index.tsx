e043beae8434091e166589d6a7a135124fad549f
Rob Dominguez <rob.dominguez@hasura.io>
2024-08-27 06:34:04 -0500

    Address review
    Addresses review suggestion by modifying the CodeStep component to be static and reverting the changes in the GuideDoc template to not use a MutationObserver.

--- src/components/CodeStep/index.tsx
+++ src/components/CodeStep/index.tsx
@@ -1,52 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import BrowserOnly from '@docusaurus/BrowserOnly';
+import React, { ReactNode } from 'react';
 import './styles.css';
 import CodeBlock from '@theme/CodeBlock';
 import { MDXProvider } from '@mdx-js/react';
 
-const CodeStep = props => {
-  const [startIndex, setStartIndex] = useState(null);
-  const [directive, setDirective] = useState('');
-  const [description, setDescription] = useState('');
+interface CodeStepProps {
+  language: string;
+  code: string;
+  heading: string;
+  children?: ReactNode;
+  output?: string;
+}
 
-  useEffect(() => {
-    if (props.children && props.children.length) {
-      // find the start of the description by finding the first p tag - everything before that is the directive
-      for (let i = 0; i < props.children.length; i++) {
-        if (props.children[i].props && props.children[i].type === 'p') {
-          setStartIndex(i);
-          break;
-        }
-      }
-      setDirective(props.children.slice(0, startIndex));
-      setDescription(props.children.slice(startIndex));
-    }
-  }, [startIndex]);
-
+const CodeStep = (props: CodeStepProps) => {
   return (
-    <BrowserOnly>
-      {() => (
-        <div className={'step_container'}>
-          <div className={'item'}>
-            <div className={'heading'}>
-              <MDXProvider children={directive} />
-            </div>
-            <div className={'description'}>
-              <MDXProvider children={description} />
-            </div>
-          </div>
-          <div className={'item'}>
-            <CodeBlock className={`language-${props.language} main-block`}>{props.code}</CodeBlock>
-            {props.output && (
-              <details>
-                <summary>Output</summary>
-                <CodeBlock className={`language-plaintext`}>{props.output}</CodeBlock>
-              </details>
-            )}
-          </div>
+    <div className={'step_container'}>
+      <div className={'item'}>
+        <div className={'heading'}>
+          <h2 children={props.heading} />
         </div>
-      )}
-    </BrowserOnly>
+        <div className={'description'}>
+          <MDXProvider children={props.children} />
+        </div>
+      </div>
+      <div className={'item'}>
+        <CodeBlock className={`language-${props.language} main-block`}>{props.code}</CodeBlock>
+        {props.output && (
+          <details>
+            <summary>Output</summary>
+            <CodeBlock className={`language-plaintext`}>{props.output}</CodeBlock>
+          </details>
+        )}
+      </div>
+    </div>
   );
 };
 
