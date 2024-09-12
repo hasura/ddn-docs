2aefb1e75d703a9960915acd5e9f6116c1d68872
Sean Park-Ross <sean@hasura.io>
2024-09-12 16:54:17 +0100

    WIP

--- src/theme/DocSidebarItem/utils.js
+++ src/theme/DocSidebarItem/utils.js
@@ -14,6 +14,7 @@ import GraphQLAPI from '@site/static/icons/graphql-logo.svg';
 import ProjectConfiguration from '@site/static/icons/dataflow-01.svg';
 import HasuraCLI from '@site/static/icons/terminal-square.svg';
 import Observability from '@site/static/icons/eye.svg';
+import Federation from '@site/static/icons/data_federation.svg';
 import Enterprise from '@site/static/icons/features/enterprise.svg';
 import Glossary from '@site/static/icons/box.svg';
 import Quickstart from '@site/static/icons/speedometer-04.svg';
@@ -59,8 +60,8 @@ export function addIconsToLabel(label, className) {
     case 'connectors-icon':
       icons = <Connectors />;
       break;
-    case 'plugins-icon':
-      icons = <Plugins/>;
+    case 'engine-plugins-icon':
+      icons = <Plugins />;
       break;
     case 'data-domain-modeling-icon':
       icons = <DataDomainModeling />;
@@ -79,6 +80,9 @@ export function addIconsToLabel(label, className) {
       break;
     case 'observability-icon':
       icons = <Observability />;
+      break;
+    case 'federation-icon':
+      icons = <Federation />;
       break;
     case 'enterprise-icon':
       icons = <Enterprise />;
