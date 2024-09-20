6ac8d32562ef32d12fc37f0a4f0fe85522071f05
Sean Park-Ross <sean@hasura.io>
2024-09-20 17:18:40 +0100

    Updates

--- utilities/create-docs-ticket/dist/linearClient.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.linearClient = void 0;
-const sdk_1 = require("@linear/sdk");
-require('dotenv').config();
-exports.linearClient = new sdk_1.LinearClient({
-    apiKey: process.env.LINEAR_API_KEY,
-});
